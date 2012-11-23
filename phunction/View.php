2796eb6afdf303004f51d79f6242ed9c8d64dc4d
Osjan <osjan@inbox.ru>
2012-11-23 21:27:58 +0100

    Basic support for layouts

--- phunction/View.php
+++ phunction/View.php
@@ -10,9 +10,42 @@
 class phunction_View extends phunction 
 {
 	
+	/**
+	 *	Function returns an array of defined View variables	 
+	 */
 	function Data() 
 	{
-		return get_object_vars($this);
+		return (array)get_object_vars($this);
+	}
+	
+	/**
+	 *	Sets or gets path to layout. 
+	 *	Layout file should be a php file containing $innerContent variable which will be replaced by the actual view content.
+	 *	This works only while using ph()->View->Render().
+	 */
+	function Layout($path=null) 
+	{
+		static $layout;
+		if ($path === null && isset($layout)) return $layout;
+		else if ($path && is_file(($path = str_replace('::', DIRECTORY_SEPARATOR, $path)) . '.php') === true) $layout = $path;
+		else $layout = null;
+	}
+	
+	/**
+	 *	Returns view file rendered inside current layout.
+	 */
+	function Render($path, $minify=false, $return=false) 
+	{
+		$layout = ph()->View->Layout();
+		
+		if ($layout === null) return ph()->View($path,null,$minify,$return);
+		
+		return ph()->View(
+			$layout, 
+			array('innerContent' => ph()->View($path,null,$minify,true)) + ph()->View->Data(), 
+			$minify, 
+			$return
+		);
 	}
 	
 }
