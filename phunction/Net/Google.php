5b0e3beef192097b1339529034d6ffaae0d32657
Osjan <osjan@inbox.ru>
2012-11-24 01:12:07 +0100

    Updated phunction_Net_Google::Rank()

--- phunction/Net/Google.php
+++ phunction/Net/Google.php
@@ -161,20 +161,85 @@ class phunction_Net_Google extends phunction_Net
 
 	public static function Rank($url)
 	{
-		if ((($url = parent::URL($url)) !== false) && (($result = parent::CURL('http://snurl.com/1invai', array('string' => $url))) !== false))
-		{
+		/**
+		 *	Using Google PageRank Checksum Algorithm in PHP by überskill
+		 *	See: https://gist.github.com/3297664
+		 */
+		 
+		$fch = 
+			function($csm)
+			{
+				if($csm < 0) 
+					$csm += 4294967296.0;			 
+				$a = (int)fmod($csm, 10);
+				$t = 1;
+				$b = (int)($csm / 10);
+				while($b) {
+					$c = $b % 10;
+					if($t)
+						$c = (int)($c / 5) + ($c * 2) % 10;
+					$b = (int)($b / 10);
+					$a += $c;
+					$t ^= 1;
+				}
+				 
+				$a = 10 - $a % 10;
+				if($a == 10)
+					return ord('0');
+				 
+				if($t)
+					return ord('0') + (int)(($a & 1 ? $a + 9 : $a) / 2);
+			 
+				return ord('0') + $a;
+			};
+		 
+		$checksum = 
+			function($str)
+			{
+				if(strlen($str) == 0)
+					return 0x1000;
+			 
+				$a = 5381.0;
+				for($i = 0; $i < strlen($str); $i++)
+					$a = fmod($a + ($a * 32) + ord($str[$i]), 4294967296.0);
+				if($a > 2147483647.0)
+					$a -= 4294967296.0;
+				$a = (int)$a;
+			 
+				$b = 0.0;
+				for($i = 0; $i < strlen($str); $i++)
+					$b = fmod(($b * 64) + ($b * 65536) - $b + ord($str[$i]), 4294967296.0);
+				if($b > 2147483647.0)
+					$b -= 4294967296.0;
+				$b = (int)$b;
+				 
+				$a = (($a >> 6) & 0x3ffffc0) | (($a >> 2) & 0x3f);
+				$c = (($a >> 4) & 0x3ffc00) | ($a & 0x3ff);
+				$d = (($c >> 4) & 0x3c000) | ($c & 0x3fff);
+				$c = ((($d & 0x3c0) << 4) | ($d & 0x3c)) << 2;
+				$a = $b & 0x0f0f;
+				$e = $b & 0x0f0f0000;
+				$b = (($d & 0xffffc000) << 4) | ($d & 0x3c00);
+			 
+				return ($b << 10) | $c | $a | $e;
+			};
+		
+		if (($url = parent::URL($url)) !== false)
+		{		
+			$csm = $checksum($url);
+			
 			$data = array
 			(
-				'ch' => ph()->Text->Regex($result, '<pre>([0-9]+)</pre>', array(1, 0)),
-				'client' => 'navclient-auto-ff',
-				'features' => 'Rank',
+				'client' => 'navclient-auto',
+				'features' => 'Rank',				
 				'q' => 'info:' . $url,
+				'ch' => sprintf('7%c%u',$fch($csm),$csm)
+				
 			);
-
-			return intval(ltrim(strrchr(parent::CURL('http://toolbarqueries.google.com/search', $data), ':'), ':'));
+			return intval(ltrim(strrchr(parent::CURL('http://toolbarqueries.google.com/tbr', $data), ':'), ':'));
 		}
-
-		return false;
+		
+		return false;	 
 	}
 
 	public static function reCAPTCHA($api)
